283700c4e75d6ca872f5d21d8690c2088935d1ab
Pries04 <121163554+Pries04@users.noreply.github.com>
2023-01-25 17:45:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-priya-app-299jyj-wjjedh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-priya-app-299jyj-wjjedh)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { recipeitemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ 
+    AppComponent , 
+    HeaderComponent, 
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    recipeitemComponent, 
+    ShoppingListComponent,
+    ShoppingEditComponent]
+})
+export class AppModule { }
